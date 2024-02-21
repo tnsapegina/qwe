9440233f1339f5203a2f24c865b905896a73c5eb
tnsapegina <143860753+tnsapegina@users.noreply.github.com>
2024-02-21 20:32:33 +0500

    Create Actor.java

---
+++ Actor.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public abstract class Actor implements ActorBehaviour{
+    protected String name;
+    protected boolean makeOrder;
+    protected boolean takeOrder;
+
+    public Actor(String name) {
+        this.name = name;
+        makeOrder = false;
+        takeOrder = false;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
